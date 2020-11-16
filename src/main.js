6dac2c6f7e4244fba2acfd67b4c6bd5051cda2d4
Jerrod <jlankford@salesforce.com>
2020-11-16 12:02:43 -0500

    Change user agent to firefox, works for login and calling

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function createWindow() {
         }
     })
     win.removeMenu();
-    win.loadURL('https://voice.google.com', { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36' });
+    win.loadURL('https://voice.google.com', { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:82.0) Gecko/20100101 Firefox/82.0' });
     // win.webContents.openDevTools();
 
     if (currentInterval) {
