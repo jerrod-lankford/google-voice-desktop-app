b6da91ea6a0d98f87a1dcf10865268b9eec84abc
Bryce Monaghan <snowcool776@gmail.com>
2020-11-23 12:36:11 -0500

    v1.0.5

--- src/preload.js
+++ src/preload.js
@@ -17,3 +17,28 @@ Notification = function (title, options) {
 Notification.prototype = OldNotification.prototype;
 Notification.permission = OldNotification.permission;
 Notification.requestPermission = OldNotification.requestPermission;
+const { remote } = require('electron')
+
+if (process.platform == 'darwin') {
+  const { systemPreferences } = remote
+
+  const setOSTheme = () => {
+    let theme = systemPreferences.isDarkMode() ? 'dark' : 'light'
+    window.localStorage.os_theme = theme
+
+    //
+    // Defined in index.html, so undefined when launching the app.
+    // Will be defined for `systemPreferences.subscribeNotification` callback.
+    //
+    if ('__setTheme' in window) {
+      window.__setTheme()
+    }
+  }
+
+  systemPreferences.subscribeNotification(
+    'AppleInterfaceThemeChangedNotification',
+    setOSTheme,
+  )
+
+  setOSTheme()
+}
